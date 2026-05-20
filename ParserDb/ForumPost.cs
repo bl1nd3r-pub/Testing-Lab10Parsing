5d4b69f503f78d464139447ab0ed4c898da3162f
babikovap <babikovap@mer.ci.nsu.ru>
2026-05-20 14:25:50 +0700

    library import

---
+++ ParserDb/ForumPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserDb
+{
+    public class ForumPost { 
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
